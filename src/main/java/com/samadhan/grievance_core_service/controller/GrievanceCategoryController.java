f1b6703b018260966e32f4d5b1ec8980134a18ab
shrutiverma2208 <123474766+shrutiverma2208@users.noreply.github.com>
2026-01-25 18:40:23 +0530

    Created Structure for greiveince catogry

---
+++ src/main/java/com/samadhan/grievance_core_service/controller/GrievanceCategoryController.java
@@ -0,0 +1,4 @@
+package com.samadhan.grievance_core_service.controller;
+
+public class GrievanceCategoryController {
+}
---
+++ src/main/java/com/samadhan/grievance_core_service/service/GrievanceCategoryServiceImpl.java
@@ -0,0 +1,4 @@
+package com.samadhan.grievance_core_service.service;
+
+public class GrievanceCategoryServiceImpl {
+}
