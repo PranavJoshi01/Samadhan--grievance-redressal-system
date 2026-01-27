49ad83196ce0f1312633cabf5a61b89b62ab2394
shrutiingale89-hash~ <shruti Ingale>
2026-01-27 22:26:31 +0530

    Add department complete

--- src/main/java/com/samadhan/grievance_core_service/controller/GrievanceCategoryController.java
+++ src/main/java/com/samadhan/grievance_core_service/controller/GrievanceCategoryController.java
@@ -2,10 +2,12 @@ package com.samadhan.grievance_core_service.controller;
 
 
 import com.samadhan.grievance_core_service.entity.GrievanceCategory;
+import com.samadhan.grievance_core_service.service.DepartmentService;
 import com.samadhan.grievance_core_service.service.GrievanceCategoryService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -60,13 +62,19 @@ public class GrievanceCategoryController {
     grievanceCategoryService.deleteCategory(id);
     return ResponseEntity.noContent().build();
     }
+    
+    
+    
+    
+    @PostMapping("/add-department")
+    public ResponseEntity<GrievanceCategory> addDepartment(
+            @RequestBody GrievanceCategory department
+    ) {
+        GrievanceCategory saved =
+                grievanceCategoryService.createCategory(department);
 
-
-
-
-
-
-
-
-
+        return new ResponseEntity<>(saved, HttpStatus.CREATED);
+    }
+    
+    
 }
