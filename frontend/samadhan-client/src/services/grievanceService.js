4e7ece71bd40d18ca864c79f7c4ead03197b3d98
shrutiverma2208 <123474766+shrutiverma2208@users.noreply.github.com>
2026-01-28 17:56:19 +0530

    adding token in header

--- frontend/samadhan-client/src/services/grievanceService.js
+++ frontend/samadhan-client/src/services/grievanceService.js
@@ -5,6 +5,18 @@ const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
   timeout: 10000,
 });
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 /**
  * Fetch all grievance categories
